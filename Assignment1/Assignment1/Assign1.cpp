d0297a8efde78e667f606658bfba1ccc0dc9b00e
leeken1985 <yukimi1030>
2013-03-02 14:19:51 -0800

    Command line args function

--- Assignment1/Assignment1/Assign1.cpp
+++ Assignment1/Assignment1/Assign1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -11,6 +12,7 @@ vector<string> split(const string& s, const string& delim);
 int main(int argc, char* argv[])
 {
 	string input;
+	vector<string> commandArgs;
 
 	if(argc > 2){
 		cerr<<"There are too many arguments"<<endl;
@@ -23,43 +25,51 @@ int main(int argc, char* argv[])
 		if(iss >> input){
 			for (vector<string>::size_type i = 0; i < split(input, ",").size(); i++)
 			{
-				cout << split(input, ",")[i] << endl;
-				if(!is_arg_valid(split(input, ",")[i]))
-					cerr<<"The arguments are invalid"<<endl;
+				if(is_arg_valid(split(input, ",")[i]))
+				{
+					commandArgs.push_back(split(input, ",")[i]);
+					cout << "This is valid: " << split(input, ",")[i] << endl;
+				}
 				else
-					cout<<"this is fine"<<endl;
+				{
+					cerr << "Command line arguments is not valid" << endl;
+					return 2;
+				}
 			}
 		}
 	}
 	return 0;
 }
 
-bool is_arg_valid(const std::string& s)
+bool is_arg_valid(const string& s)
 {
-    string::const_iterator it = s.begin();
-	
-    while (it != s.end())
+	static set<int> myValidation;
+	pair<set<int>::iterator,bool> ret;
+	int num;
+	string currValue;
+	istringstream iss(s);
+	iss >> num;
+	istringstream iss1(num);
+	iss >> currValue;
+	if(currValue.size() == s.size())
 	{
-		//if(*it == 0)
-		//	return 0;
-		//else if(*it == ' ')
-		//{
-		//	return 0;
-		//}
-		//else if(*it == '-')
-		//{
-		//	if(!isdigit(*(++it)))
-		//		return 0;
-		//}
-		//else if(isdigit((*it)))
-		//{
-		//	if(it != s.end())
-		//		if(*it == 't')
-		//}
-		cout << *it << ' ' << endl;
-		
+		ret = myValidation.insert((num * -1));
+		if(ret.second == false)
+			return false;
+		else
+			return true;
 	}
-	return !s.empty() && it == s.end();
+	else if(s.size() - currValue.size() == 1)
+	{
+		string::const_iterator it = s.end()-1;
+		ret = myValidation.insert((num * -1));
+		if(ret.second == false)
+			return false;
+		if(*it == 't')
+			return true;
+	}
+	else
+		return false;
 }
 
 vector<string> split(const string& s, const string& delim)
